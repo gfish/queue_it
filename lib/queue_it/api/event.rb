faca88475d8b6d230fde5f77c3cd629cac5e48e9
Szymon Fiedler <fidelski@gmail.com>
2015-04-23 23:23:13 +0200

    Basic implementation of Event creation/update
    This is a MVP covering our project needs.
    It's not yet operational, since only in memory QueueIt simulator is
    operational.

---
+++ lib/queue_it/api/event.rb
@@ -0,0 +1,99 @@
+require 'json'
+require 'uri'
+
+module QueueIt
+  module Api
+    class Event
+      def initialize(connection)
+        self.connection = connection
+      end
+
+      def create_or_update(event_id:, display_name:, start_time:, know_user_secret_key:, redirect_url:, end_time: nil, description: nil, max_redirects_per_minute: 15, event_culture_name: "en-GB", time_zone: "UTC", queue_number_validity_in_minutes: 15)
+        attributes = queue_attributes(
+          start_time:                       start_time,
+          end_time:                         end_time,
+          know_user_secret_key:             know_user_secret_key,
+          max_redirects_per_minute:         max_redirects_per_minute,
+          redirect_url:                     redirect_url,
+          description:                      description,
+          display_name:                     display_name,
+          event_culture_name:               event_culture_name,
+          queue_number_validity_in_minutes: queue_number_validity_in_minutes,
+          time_zone:                        time_zone)
+
+        perform_request(:put, event_id, attributes)
+      end
+
+      private
+
+      attr_accessor :connection
+
+      ONE_YEAR        = 31557600.freeze
+      ONE_HOUR        = 3600.freeze
+      MD5HashSecurity = 5.freeze
+      UseSSLAuto      = 2.freeze
+
+      MICROSOFT_TIME_ZONE_INDEX_VALUES = {
+        "Europe/Copenhagen" => "Romance Standard Time",
+        "Europe/Helsinki"   => "FLE Standard Time",
+        "Europe/London"     => "GMT Standard Time",
+        "Europe/Paris"      => "GMT Standard Time",
+        "Paris"             => "Romance Standard Time",
+        "Stockholm"         => "W. Europe Standard Time",
+      }.freeze
+
+      def utc_start_time(start_time)
+        start_time.to_i
+      end
+
+      def utc_end_time(start_time, end_time)
+        end_time && end_time.to_i || utc_start_time(start_time) + ONE_YEAR
+      end
+
+      def pre_queue_start_time(start_time)
+        start_time.to_i - ONE_HOUR
+      end
+
+      def translate_time_zone(time_zone)
+        MICROSOFT_TIME_ZONE_INDEX_VALUES.fetch(time_zone, time_zone)
+      end
+
+      def queue_attributes(start_time:, end_time:, know_user_secret_key:, max_redirects_per_minute:, redirect_url:, description:, display_name:, event_culture_name:, queue_number_validity_in_minutes:, time_zone:)
+        {
+          "AfterEventLogic"              =>2,
+          "AfterEventRedirectPage"       =>nil,
+          "AllowedCustomLayouts"         =>[],
+          "BrowserCultureEnabled"        =>true,
+          "CustomLayout"                 =>"HighLoad layout",
+          "Description"                  =>description,
+          "DisplayName"                  =>display_name,
+          "DomainAlias"                  =>"String content",
+          "EventCultureName"             =>event_culture_name,
+          "EventEndTime"                 =>"/Date(#{utc_end_time(start_time, end_time)})/",
+          "EventStartTime"               =>"/Date(#{utc_start_time(start_time)})/",
+          "JavaScriptSupportEnabled"     =>false,
+          "KnowUserSecretKey"            =>know_user_secret_key,
+          "KnowUserSecurity"             =>MD5HashSecurity,
+          "MaxNoOfRedirectsPrQId"        =>1,
+          "MaxRedirectsPerMinute"        =>max_redirects_per_minute,
+          "PreQueueStartTime"            =>"/Date(#{pre_queue_start_time(start_time)})/",
+          "QueueNumberValidityInMinutes" =>queue_number_validity_in_minutes,
+          "RedirectUrl"                  =>URI(redirect_url).to_s,
+          "ReportPerformanceCounters"    =>true,
+          "SafetyNetAverageMinutes"      =>3,
+          "SafetyNetEvent"               =>false,
+          "TargetUrlSupportEnabled"      =>false,
+          "TargetUrlValidationRegex"     =>"",
+          "TimeZone"                     =>translate_time_zone(time_zone),
+          "UseSSL"                       =>UseSSLAuto,
+          "XUsersInFrontOfYou"           =>100
+        }
+      end
+
+      def perform_request(method, path, body = {})
+        json_response = connection.public_send(method, path, body)
+        json_response.body
+      end
+    end
+  end
+end
---
+++ spec/queue_it/api/event_spec.rb
@@ -0,0 +1,112 @@
+require 'queue_it/api/fake_connection'
+require 'queue_it/api/event'
+
+module QueueIt
+  module Api
+    describe Event do
+      let(:connection) { FakeConnection.new(api_key: "VALID_KEY", endpoint_url: "https://api.queue-it.net/1.2/event") }
+
+      subject(:event_adapter) { Event.new(connection) }
+
+      context "#create_or_update" do
+        specify "creates or updates event" do
+          expect(event_adapter.create_or_update(event_id:             "fancy_event",
+                                                display_name:         "Fancy Event 2015",
+                                                description:          "Foo",
+                                                redirect_url:         "https://example.com/en/events/fancy_event/tickets",
+                                                start_time:           Time.new(2015,04,28,17,25,46, "+02:00"),
+                                                end_time:             Time.new(2015,04,28,21,25,46, "+02:00"),
+                                                know_user_secret_key: "930f42ca-d9e7-4202-bff4-606e127b1c103980c131-cd8a-4e35-a945-50f7b5102ad6",
+                                                max_redirects_per_minute: 15,
+                                                queue_number_validity_in_minutes: 15))
+            .to eq(expected_create_response)
+        end
+
+        specify "minimum viable attributes for creating event" do
+          end_time = Time.utc(2016,04,27,21,25,46).to_i
+          response = expected_create_response.merge({
+              "Description"       => nil,
+              "EventEndTime"      => "/Date(#{end_time})/",
+              "EventEndLocalTime" => "/Date(#{end_time}+0000)/",
+            })
+
+          expect(event_adapter.create_or_update(event_id:             "fancy_event",
+                                                display_name:         "Fancy Event 2015",
+                                                redirect_url:         "https://example.com/en/events/fancy_event/tickets",
+                                                start_time:           Time.utc(2015,04,28,15,25,46),
+                                                know_user_secret_key: "930f42ca-d9e7-4202-bff4-606e127b1c103980c131-cd8a-4e35-a945-50f7b5102ad6",))
+            .to eq(response)
+        end
+
+        specify "set custom time zone in the ruby way" do
+          response = expected_create_response.merge({ "TimeZone" => "Romance Standard Time" })
+          expect(event_adapter.create_or_update(event_id:                          "fancy_event",
+                                                display_name:                      "Fancy Event 2015",
+                                                description:                       "Foo",
+                                                redirect_url:                      "https://example.com/en/events/fancy_event/tickets",
+                                                start_time:                        Time.new(2015,04,28,17,25,46, "+02:00"),
+                                                end_time:                          Time.new(2015,04,28,21,25,46, "+02:00"),
+                                                know_user_secret_key:              "930f42ca-d9e7-4202-bff4-606e127b1c103980c131-cd8a-4e35-a945-50f7b5102ad6",
+                                                max_redirects_per_minute:          15,
+                                                queue_number_validity_in_minutes:  15,
+                                                time_zone:                         "Europe/Copenhagen",))
+            .to eq(response)
+        end
+
+        pending "incorrect event_id provided" do
+          fail
+          expect do
+            event_adapter.create_or_update(event_id:             "thats not a proper id 99",
+                                           display_name:         "Fancy Event 2015",
+                                           description:          "Foo",
+                                           redirect_url:         "https://example.com/en/events/fancy_event/tickets",
+                                           start_time:           Time.new(2015,04,28,17,25,46, "+02:00"),
+                                           end_time:             Time.new(2015,04,28,21,25,46, "+02:00"),
+                                           know_user_secret_key: "930f42ca-d9e7-4202-bff4-606e127b1c103980c131-cd8a-4e35-a945-50f7b5102ad6",
+                                           max_redirects_per_minute: 15,
+                                           queue_number_validity_in_minutes: 15)
+          end.to raise_error(Event::ClientError)
+        end
+      end
+
+      private
+
+      def expected_create_response
+        {
+          "AfterEventLogic"=>2,
+          "AfterEventRedirectPage"=>nil,
+          "AllowedCustomLayouts"=>[],
+          "BrowserCultureEnabled"=>false,
+          "CustomLayout"=>nil,
+          "Description"=>"Foo",
+          "DisplayName"=>"Fancy Event 2015",
+          "DomainAlias"=>"fancy_event-testcompany.queue-it.net",
+          "EventCultureName"=>"en-GB",
+          "EventEndTime"=>"/Date(1430249146)/",
+          "EventStartTime"=>"/Date(1430234746)/",
+          "JavaScriptSupportEnabled"=>false,
+          "KnowUserSecretKey"=>"930f42ca-d9e7-4202-bff4-606e127b1c103980c131-cd8a-4e35-a945-50f7b5102ad6",
+          "KnowUserSecurity"=>5,
+          "MaxNoOfRedirectsPrQId"=>1,
+          "MaxRedirectsPerMinute"=>15,
+          "PreQueueStartTime"=>"/Date(1430231146)/",
+          "QueueNumberValidityInMinutes"=>15,
+          "RedirectUrl"=>"https://example.com/en/events/fancy_event/tickets",
+          "ReportPerformanceCounters"=>false,
+          "SafetyNetAverageMinutes"=>3,
+          "SafetyNetEvent"=>false,
+          "TargetUrlSupportEnabled"=>true,
+          "TargetUrlValidationRegex"=>"",
+          "TimeZone"=>"UTC",
+          "UseSSL"=>2,
+          "XUsersInFrontOfYou"=>100,
+          "EventEndLocalTime"=>"/Date(1430249146+0000)/",
+          "EventId"=>"fancy_event",
+          "EventStartLocalTime"=>"/Date(1430234746+0000)/",
+          "PreQueueStartLocalTime"=>"/Date(1430231146+0000)/",
+          "QueueUrl"=>"http://fancy_event-testcompany.queue-it.net/?c=testcompany&e=fancy_event"
+        }
+      end
+    end
+  end
+end
