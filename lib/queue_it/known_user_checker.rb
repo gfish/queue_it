313175f0a62b05fff871ca008aefc1f4269df3f5
Rafał Łasocha <me@swistak35.com>
2017-03-15 00:35:38 +0100

    Add more tests to KnownUserChecker

--- lib/queue_it/known_user_checker.rb
+++ lib/queue_it/known_user_checker.rb
@@ -3,12 +3,10 @@ require 'digest/md5'
 module QueueIt
   class KnownUserChecker
     def call(secret_key:, request_url:, request_params:)
-      queue_id = request_params['q'] # A QuID, the user’s queue ID
-      encrypted_place_in_queue = request_params['p'] # A text, an encrypted version of the user’s queue number
-      expected_hash = request_params['h'] # An integer calculated hash
-      timestamp = request_params['ts'] # An integer timestamp counting number of seconds since 1970-01-01 00:00:00 UTC
+      encrypted_place_in_queue = request_params['p']
+      expected_hash = request_params['h']
 
-      raise QueueIt::MissingArgsGiven.new if [ request_url, queue_id, encrypted_place_in_queue, timestamp, expected_hash ].any?(&:nil?)
+      raise QueueIt::MissingArgsGiven.new if queue_it_params_missing?(request_params)
 
       if verify_md5_hash?(request_url, expected_hash, secret_key)
         return decrypted_place_in_queue(encrypted_place_in_queue)
@@ -18,6 +16,15 @@ module QueueIt
     end
 
     private
+
+    def queue_it_params_missing?(params)
+      queue_id = params['q'] # A QuID, the user’s queue ID
+      encrypted_place_in_queue = params['p'] # A text, an encrypted version of the user’s queue number
+      expected_hash = params['h'] # An integer calculated hash
+      timestamp = params['ts'] # An integer timestamp counting number of seconds since 1970-01-01 00:00:00 UTC
+
+      [queue_id, encrypted_place_in_queue, timestamp, expected_hash].any?(&:nil?)
+    end
 
     # uses one char of each string at a given starting point
     # given b852fe78-0d10-4254-823c-f8749c401153 should get 4212870
--- spec/queue_it/known_user_checker_spec.rb
+++ spec/queue_it/known_user_checker_spec.rb
@@ -23,6 +23,38 @@ module QueueIt
       end.to raise_error(MissingArgsGiven)
     end
 
+    specify "queue id param is required" do
+      url = "https://example.com/some/path?p=053eeb2c-b272-41a2-aacf-2742bc99676c&ts=1489367379&c=examplecompany&e=someeventid42&rt=Queue&h=bbaf9807496ecb687c85bfcc1a8369e1"
+
+      expect do
+        service.(secret_key: secret_key, request_url: url, request_params: parse_params(url))
+      end.to raise_error(MissingArgsGiven)
+    end
+
+    specify "timestamp param is required" do
+      url = "https://example.com/some/path?q=2647344b-e639-4cd6-8a77-3a8801553716&p=053eeb2c-b272-41a2-aacf-2742bc99676c&c=examplecompany&e=someeventid42&rt=Queue&h=bbaf9807496ecb687c85bfcc1a8369e1"
+
+      expect do
+        service.(secret_key: secret_key, request_url: url, request_params: parse_params(url))
+      end.to raise_error(MissingArgsGiven)
+    end
+
+    specify "encrypted place in queue param is required" do
+      url = "https://example.com/some/path?q=2647344b-e639-4cd6-8a77-3a8801553716&ts=1489367379&c=examplecompany&e=someeventid42&rt=Queue&h=bbaf9807496ecb687c85bfcc1a8369e1"
+
+      expect do
+        service.(secret_key: secret_key, request_url: url, request_params: parse_params(url))
+      end.to raise_error(MissingArgsGiven)
+    end
+
+    specify "hash is required" do
+      url = "https://example.com/some/path?q=2647345b-e639-4cd6-8a77-3a8801553716&p=053eeb2c-b272-41a2-aacf-2742bc99676c&ts=1489367379&c=examplecompany&e=someeventid42&rt=Queue"
+
+      expect do
+        service.(secret_key: secret_key, request_url: url, request_params: parse_params(url))
+      end.to raise_error(MissingArgsGiven)
+    end
+
     def parse_params(url)
       CGI.parse(URI.parse(url).query).each_with_object({}) {|(k,v),o| o[k] = v.first }
     end
